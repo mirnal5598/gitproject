d718a47a14d0fe29e20420a2fff8ef00784f9b88
mirnal <mirnal@tothenew.com>
2022-03-17 00:41:29 +0530

    removing coupon services issue file

--- coupon-microservices/src/main/java/com/example/demo/repos/CouponRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repos;
-
-import com.example.demo.model.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepo extends JpaRepository<Coupon, Integer> {
-    Coupon findByCode(String code);
-}
--- coupon-microservices/src/main/java/com/example/demo/repos/RoleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repos;
-
-import com.example.demo.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Integer> {
-}
